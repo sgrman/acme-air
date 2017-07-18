a4952d2c491f9bdb3f0fd816b3fdab141df9c3ea
pouya <pouya@applariat.com>
2017-07-18 14:43:19 -0700

    updated deploy.sh to use commit for artifact download

--- deploy.sh
+++ deploy.sh
@@ -42,7 +42,8 @@ cat >stack-artifact.yaml <<EOL
 loc_artifact_id: ${APL_LOC_ARTIFACT_ID}
 stack_id: ${APL_STACK_ID}
 stack_artifact_type: code
-artifact_name: https://github.com/applariat/acme-air/archive/${TRAVIS_TAG}.zip
+#artifact_name: https://github.com/applariat/acme-air/archive/${TRAVIS_TAG}.zip
+artifact_name: https://github.com/applariat/acme-air/archive/${TRAVIS_COMMIT}.zip
 name: ${APL_ARTIFACT_NAME}
 EOL
 
