e78bcdee19dbe6ed3a239f32f7ebfc6fb52296ad
Chris Dornsife <chris@dornsife.com>
2017-07-19 16:52:51 -0700

    added release command and changed to use parameters instead of files for stack artifact and deploy calls

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ set -e # Exit with nonzero exit code if anything fails
 APL_LOC_DEPLOY_ID=${APL_LOC_DEPLOY_ID:?Missing required env var}
 APL_LOC_ARTIFACT_ID=${APL_LOC_ARTIFACT_ID:?Missing required env var}
 APL_STACK_ID=${APL_STACK_ID:?Missing required env var}
-APL_RElEASE_ID=${APL_RElEASE_ID:?Missing required env var}
+APL_STACK_VERSION_ID=${APL_STACK_VERSION_ID:?Missing required env var}
+APL_RELEASE_ID=${APL_RELEASE_ID:?Missing required env var}
 APL_STACK_COMPONENT_ID=${APL_STACK_COMPONENT_ID:?Missing required env var}
 APL_ARTIFACT_NAME=${APL_ARTIFACT_NAME:?Missing required env var}
 APL_CMD_RELEASE=${APL_CMD_RELEASE:-v0.1.0}
@@ -18,12 +19,12 @@ echo
 if [ ! -z "$TRAVIS_TAG" ]; then
     APL_ARTIFACT_NAME="STAGING-${TRAVIS_TAG}"
     CODE_LOC=${TRAVIS_TAG}
-    QOS_LEVEL=level5
+    WORKLOAD_TYPE=level5
 else
     TRAVIS_COMMIT=`echo $TRAVIS_COMMIT |cut -c 1-12`
     APL_ARTIFACT_NAME="QA-${TRAVIS_COMMIT}"
     CODE_LOC=${TRAVIS_COMMIT}
-    QOS_LEVEL=level2
+    WORKLOAD_TYPE=level2
 fi
 
 #APL_ARTIFACT_NAME="${APL_ARTIFACT_NAME}-${TRAVIS_BUILD_NUMBER}"
@@ -31,32 +32,28 @@ fi
 #APL_ARTIFACT_NAME="QA-${TRAVIS_COMMIT}"
 
 
-# Make the name domain safe. // TODO: The API should handle this
-APL_ARTIFACT_NAME=${APL_ARTIFACT_NAME//[^A-Za-z0-9\\-]/-}
-
-APL_FILE=apl-${APL_CMD_RELEASE}-linux_amd64.tgz
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    APL_FILE=apl-${APL_CMD_RELEASE}-darwin_amd64.tgz
-fi
-echo
-echo "Downloading cli: https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}"
-wget -q https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}
-tar zxf ${APL_FILE}
-
-# Create the stack-artifact yaml to submit.
-cat >stack-artifact.yaml <<EOL
-loc_artifact_id: ${APL_LOC_ARTIFACT_ID}
-stack_id: ${APL_STACK_ID}
-stack_artifact_type: code
-artifact_name: https://github.com/applariat/acme-air/archive/${CODE_LOC}.zip
-name: ${APL_ARTIFACT_NAME}
-EOL
+## Make the name domain safe. // TODO: The API should handle this
+#APL_ARTIFACT_NAME=${APL_ARTIFACT_NAME//[^A-Za-z0-9\\-]/-}
+#
+#APL_FILE=apl-${APL_CMD_RELEASE}-linux_amd64.tgz
+#if [[ "$OSTYPE" == "darwin"* ]]; then
+#    APL_FILE=apl-${APL_CMD_RELEASE}-darwin_amd64.tgz
+#fi
+#echo
+#echo "Downloading cli: https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}"
+#wget -q https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}
+#tar zxf ${APL_FILE}
 
 echo
 echo "Submitting stack artifact file:"
-cat stack-artifact.yaml
 
-APL_SA_CREATE_RESULT_JSON=$(./apl stack-artifacts create -f stack-artifact.yaml -o json)
+APL_SA_CREATE_RESULT_JSON=$(./apl stack-artifacts create \
+    --loc-artifact-id ${APL_LOC_ARTIFACT_ID} \
+    --stack-id ${APL_STACK_ID} \
+    --stack-artifact-type code \
+    --artifact-name https://github.com/applariat/acme-air/archive/${CODE_LOC}.zip \
+    --name ${APL_ARTIFACT_NAME} \
+    -o json)
 
 echo
 echo "Result: ${APL_SA_CREATE_RESULT_JSON}"
@@ -74,67 +71,58 @@ echo "Stack Artifact ID: ${APL_STACK_ARTIFACT_ID}"
 
 if [ ! -z "$TRAVIS_TAG" ]; then
 cat >release.yaml <<EOL
-name: ${APL_ARTIFACT_NAME}
+meta_data:
+    display_name: ${APL_ARTIFACT_NAME}
 stack_id: ${APL_STACK_ID}
-project_id: ${APL_PROJECT_ID}
-
+stack_version_id: ${APL_STACK_VERSION_ID}
 components:
-- name: mongo
-    services:
-    - name: mongo-service
-      component_service_id: ct-stateful-mongo
-      release:
-        artifacts:
-          image:
-            stack_artifact_id: 5069daa9-a30f-4434-bf7c-435060b1c973
-        runBuild: false
-        buildvars: []
-    stack_component_id: a11d7518-81ba-4364-af6d-951acb5c70f9
   - name: node-svc
+    stack_component_id: ${APL_STACK_COMPONENT_ID}
     services:
     - name: node-service
-      default_configuration: build
-      component_service_id: ct-deployment
       release:
         artifacts:
-          builder:
-            stack_artifact_id: e61131ca-4ff6-43ba-87a8-a3b6b1c14e2f
           code:
-            stack_artifact_id: 28934fdc-a884-4d52-91ef-ca126674debd
-          image:
-            stack_artifact_id: 8b1cc91f-2471-4fec-8e3d-74c45fb198a2
-        runBuild: true
-        buildvars:
-        - value: 0
-          key: REBUILD_NUM
-    stack_component_id: 347409b8-d00e-4e85-9dc2-8cf56138b821
+            stack_artifact_id: ${APL_STACK_ARTIFACT_ID}
 EOL
-fi
 
-cat >deploy.yaml <<EOL
-name: ${APL_ARTIFACT_NAME}
-release_id: ${APL_RElEASE_ID}
-loc_deploy_id: ${APL_LOC_DEPLOY_ID}
-lease_type: temporary
-workload_type : ${QOS_LEVEL}
-lease_period_days: 6
-components:
-- stack_component_id: ${APL_STACK_COMPONENT_ID}
-  services:
-  - component_service_id: ct-deployment
-    name: node-service
-    overrides:
-      build:
-        artifacts:
-          code: ${APL_STACK_ARTIFACT_ID}
-EOL
+#    APL_RELEASE_CREATE_RESULT_JSON=$(./apl releases create \
+#        --name ${APL_ARTIFACT_NAME} \
+#        --stack-id ${APL_STACK_ID} \
+#        --stack-version-id ${APL_STACK_VERSION_ID} \
+#        --service-name node-service \
+#        --stack-artifact-id ${APL_STACK_ARTIFACT_ID} \
+#        --stack-component-id ${APL_STACK_COMPONENT_ID} \
+#        -o json)
+
+    APL_RELEASE_CREATE_RESULT_JSON=$(./apl releases create -f release.yaml -o json)
+
+    echo
+    echo "Result: ${APL_RELEASE_CREATE_RESULT_JSON}"
+    if [ $? -ne 0 ]
+    then
+        echo $APL_RELEASE_CREATE_RESULT_JSON | jq -r '.message'
+        exit 1
+    fi
+
+    APL_RELEASE_ID=$(echo $APL_RELEASE_CREATE_RESULT_JSON | jq -r '.data')
+    echo "Release ID: ${APL_RELEASE_ID}"
+fi
 
 echo
 echo "Submitting deployment:"
-cat deploy.yaml
 
 # deploy it
-APL_DEPLOY_CREATE_RESULT_JSON=$(./apl deployments create -f deploy.yaml -o json)
+APL_DEPLOY_CREATE_RESULT_JSON=$(./apl deployments create \
+    --loc-deploy-id ${APL_LOC_DEPLOY_ID} \
+    --name ${APL_ARTIFACT_NAME} \
+    --workload-type ${WORKLOAD_TYPE} \
+    --release-id ${APL_RELEASE_ID} \
+    --stack-component-id ${APL_STACK_COMPONENT_ID} \
+    --component-service-id ct-deployment \
+    --service-name  node-service \
+    --stack-artifact-id ${APL_STACK_ARTIFACT_ID} \
+    -o json)
 
 echo
 echo "Result: ${APL_DEPLOY_CREATE_RESULT_JSON}"
