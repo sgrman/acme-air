91937a5c7d56524636f0a24d1feea5a996f86670
pouya <pouya@applariat.com>
2017-07-19 13:23:45 -0700

    testing new cli 3rd

--- deploy.sh
+++ deploy.sh
@@ -40,7 +40,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 echo
 echo "Downloading cli: https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}"
-wget -q https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}
+wget https://github.com/applariat/go-apl/releases/download/${APL_CMD_RELEASE}/${APL_FILE}
 tar zxvf ${APL_FILE}
 
 #testing
